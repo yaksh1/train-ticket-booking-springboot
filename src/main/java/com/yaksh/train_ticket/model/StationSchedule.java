53d1d4d6bcbed9330ddc89162406ac0a130c505e
yaksh gandhi <95672067+yaksh1@users.noreply.github.com>
2025-03-17 02:44:32 +0530

    test pre-commit hook

--- src/main/java/com/yaksh/train_ticket/model/StationSchedule.java
+++ src/main/java/com/yaksh/train_ticket/model/StationSchedule.java
@@ -7,19 +7,10 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-/**
- * Represents the schedule of a station in the train ticketing system.
- * This class contains details about the station name and its arrival time.
- */
 @Data
-@AllArgsConstructor // Generates a constructor with all fields as parameters.
-@NoArgsConstructor  // Generates a no-argument constructor.
-@Builder            // Enables the builder pattern for creating instances of this class.
+@AllArgsConstructor
+@NoArgsConstructor
 public class StationSchedule {
-
-    /** The name of the station. */
     private String name;
-
-    /** The arrival time at the station. */
     private LocalDateTime arrivalTime;
 }
