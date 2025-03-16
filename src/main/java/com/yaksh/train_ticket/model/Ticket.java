22e2594489a0b5d2e203a9d1ca12ac071838e1fb
yaksh gandhi <95672067+yaksh1@users.noreply.github.com>
2025-03-17 01:42:35 +0530

    testing github action workflow v2

--- src/main/java/com/yaksh/train_ticket/model/Ticket.java
+++ src/main/java/com/yaksh/train_ticket/model/Ticket.java
@@ -11,8 +11,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
